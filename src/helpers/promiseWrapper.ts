d71800197d242c62aedb6952cf29fb761b2c3ba6
Savita Katwe <savitamerwade04@gmail.com>
2024-01-21 20:20:39 +0400

    pushed

--- src/helpers/promiseWrapper.ts
+++ src/helpers/promiseWrapper.ts
@@ -3,6 +3,20 @@ import { Request, Response } from "express";
 const promiseWrapper =
   <TRES>(callback: (req: Request) => Promise<TRES>) =>
   (req: Request, res: Response) => {
+    // @ts-ignore
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    );
+
     callback(req)
       .then((result: TRES) => {
         res.send(result).status(200);
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import routes from "./controllers/routes";
 import { dbConnection } from "./configs/databaseConnections";
 import bodyParser from "body-parser";
 import cors from "cors";
-import allowCors from "./middlewares/cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(allowCors);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 dbConnection();
